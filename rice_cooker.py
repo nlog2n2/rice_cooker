4430274e9c453b096472681d622d6e96ff8007b7
nlog2n2 <s45c.ss400@gmail.com>
2020-01-01 13:24:11 +0900

    fixed timestamp

--- rice_cooker.py
+++ rice_cooker.py
@@ -5,13 +5,19 @@ import qrcode
 from PIL import Image, ImageDraw, ImageFont
 import requests
 
+# Please Write Your Wi-Fi Setting & Slack Token & Font Path
 CONPANY_NAME        = 'GUEST'
 ENCRYPTION_METHOD   = 'WPA'
 SLACK_TOKEN         = ''
 FONT_PATH           = '/usr/share/fonts/dejavu/DejaVuSans.ttf'
 
+# Password Length & Font Size Setting
 PASSWORD_MAX_LENGTH = 16
 FONT_SIZE           = 20
+
+# PIL Size property index
+WIDTH_INDEX         = 0
+HEIGHT_INDEX        = 1
 
 #########################################################
 ## main
@@ -22,7 +28,8 @@ FONT_SIZE           = 20
 #########################################################
 
 # Create SSID & Password & Qrcode
-monthly_ssid                    = CONPANY_NAME + '{0:-%Y-%m}'.format(datetime.datetime.now())
+carete_timestamp                = datetime.datetime.now()
+monthly_ssid                    = CONPANY_NAME + '{0:-%Y-%m}'.format(carete_timestamp)
 monthly_password                = ''.join([secrets.choice(string.ascii_letters) for i in range(PASSWORD_MAX_LENGTH) ])
 wifi_conneting_infomation       = 'WIFI:T:' + ENCRYPTION_METHOD + ';S:' + monthly_ssid + ';P:' + monthly_password + ';;'
 wifi_conneting_infomation_list  = ['SSID: ' + monthly_ssid , 'Password: ' + monthly_password]
@@ -36,16 +43,20 @@ wifi_conneting_infomation_draw  = ImageDraw.Draw(base_image)
 wifi_conneting_infomation_font  = ImageFont.truetype(FONT_PATH,FONT_SIZE)
 writing_position = 0
 for printing_inromation in wifi_conneting_infomation_list:
-    wifi_conneting_infomation_draw.multiline_text((0, writing_position),
-                                                    printing_inromation,
-                                                    fill=(0, 0, 0),
-                                                    font=wifi_conneting_infomation_font)
-    writing_position = writing_position + 20
-    print(printing_inromation)
+    wifi_conneting_infomation_draw.multiline_text((0,
+                                                   writing_position),
+                                                   printing_inromation,
+                                                   fill=(0, 0, 0),
+                                                   font=wifi_conneting_infomation_font)
+    writing_position = writing_position + FONT_SIZE
 
 # Concat QR code Image & Wi-Fi Infomation Image
-height , width = qrcode_image.size
-result_image = Image.new('RGB',(width, height + base_image.height))
-result_image.paste(qrcode_image,(0,0))
-result_image.paste(base_image,(0,height))
-result_image.save('/home/vagrant/rice_cooker/result.png')
+wifi_conneting_infomation_image = Image.new('RGB',
+                                            (qrcode_image.size[WIDTH_INDEX],
+                                             qrcode_image.size[HEIGHT_INDEX] + base_image.size[HEIGHT_INDEX]))
+
+wifi_conneting_infomation_image.paste(qrcode_image,(0,0))
+wifi_conneting_infomation_image.paste(base_image,(0,qrcode_image.size[HEIGHT_INDEX]))
+wifi_conneting_infomation_image.save('/home/vagrant/rice_cooker/result.png') # 日付
+
+
