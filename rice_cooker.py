0c365de167848fdb9721a72f2059dc5e4255259d
nlog2n2 <s45c.ss400@gmail.com>
2020-01-01 11:06:57 +0900

    create saving base image function

--- rice_cooker.py
+++ rice_cooker.py
@@ -8,15 +8,11 @@ import requests
 CONPANY_NAME        = 'GUEST'
 ENCRYPTION_METHOD   = 'WPA'
 SLACK_TOKEN         = ''
-IMAGE_DIR           = os.path.curdir +'/image'
+FONT_PATH           = '/usr/share/fonts/dejavu/DejaVuSans.ttf'
+
 SSID_MAX_LENGTH     = 20
 PASSWORD_MAX_LENGTH = 16
-
-FONT_PATH           = '/usr/share/fonts/dejavu/DejaVuSans.ttf'
-FONT_SIZE           = 12
-BASE_IMAGE_WIDTH    = 20
-BASE_IMAGE_HEIGTH   = 20
-
+FONT_SIZE           = 20
 
 #########################################################
 ## main
@@ -30,29 +26,23 @@ BASE_IMAGE_HEIGTH   = 20
 monthly_ssid                    = CONPANY_NAME + '{0:-%Y-%m}'.format(datetime.datetime.now())
 monthly_password                = ''.join([secrets.choice(string.ascii_letters) for i in range(PASSWORD_MAX_LENGTH) ])
 wifi_conneting_infomation       = 'WIFI:T:' + ENCRYPTION_METHOD + ';S:' + monthly_ssid + ';P:' + monthly_password + ';;'
-
-qrcode_image                    = qrcode.make(wifi_conneting_infomation)
+wifi_conneting_infomation_list  = ['SSID: ' + monthly_ssid , 'Password: ' + monthly_password]
 
 # Saving QR Code Image
-#QRCODE_SAVING_DIR   = os.path
+qrcode_image                    = qrcode.make(wifi_conneting_infomation)
+
+# Saving Wi-Fi Infomation Image
+base_image                      = Image.new('RGB', (410, 60), 'white')
+wifi_conneting_infomation_draw  = ImageDraw.Draw(base_image)
+wifi_conneting_infomation_font  = ImageFont.truetype(FONT_PATH,FONT_SIZE)
+writing_position = 0
+for printing_inromation in wifi_conneting_infomation_list:
+    wifi_conneting_infomation_draw.multiline_text((0, writing_position),
+                                                    printing_inromation,
+                                                    fill=(0, 0, 0),
+                                                    font=wifi_conneting_infomation_font)
+    writing_position = writing_position + 20
+    print(printing_inromation)
+
 qrcode_image.save('/home/vagrant/rice_cooker/qrcode.png')
-
-
-# ase_image = create_base_image('RGB', BASE_IMAGE_WIDTH, BASE_IMAGE_HEIGTH, 'white')
-# draw = ImageDraw.Draw(base_image)
-# font = ImageFont.truetype(FONT_PATH, FONT_SIZE)
-# draw.multiline_text((0, 0), 'Pillow sample /n hoge', fill=(0, 0, 0), font=font)
-
-def create_base_image(mode, width, height, color, infomation):
-    
-    if(os.path.exists(IMAGE_DIR)):
-        os.makedirs(IMAGE_DIR)
-    
-    #BASE_IMAGE_PATH     = IMAGE_DIR + 'base.png' # todo date write
-        
-    return 0
-#Image.new('RGB', (width, height), 'white')
-
-print('SSID: ' + monthly_ssid)
-print('Password: ' + monthly_password)
-# print(wifi_conneting_infomation)
+base_image.save('/home/vagrant/rice_cooker/base.png')
